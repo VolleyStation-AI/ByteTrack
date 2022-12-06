55e41179636e51b22a493907bb639ce9af2f5255
Andrew Tao <andrew.tao@gmail.com>
2022-12-06 07:38:20 -0800

    trained new model with v5 dataset

--- train_fullcourt.sh
+++ train_fullcourt.sh
@@ -7,16 +7,19 @@ export WORLD_SIZE=1
 # unset WORLD_SIZE
 
 EXP=yolox_x_fullcourt
-DSET=v8_2
-TAG="new-bytetrack-baseline"
+DSET=v5
+TAG="bytetrack-with-bad-touches"
 
 CKPT=/mnt/g/checkpoints/ByteTrack/bytetrack_x_mot17.pth.tar
 
 python3 tools/train.py \
 	-d 2 -b 8 --fp16 \
 	-f exps/example/mot/${EXP}.py \
+	-c $CKPT \
 	-o \
         -expn ${EXP}_${DSET}${TAG} \
 	train_ann /mnt/g/data/vball/fullcourt/trn_${DSET}.json \
 	val_ann /mnt/g/data/vball/fullcourt/val_${DSET}.json \
 	max_epoch 12 no_aug_epochs 4
+
+# train_ann /mnt/g/data/vball/fullcourt/trn_${DSET}.json \
