1e68e577183bd1db510bff4dd02453f0288eca3d
Andrew Tao <andrew.tao@gmail.com>
2025-11-28 08:45:00 -0800

    fix to autocast

--- train_fullcourt.sh
+++ train_fullcourt.sh
@@ -1,25 +1,24 @@
 export PYTHONPATH=$PWD:${PWD}/../multi/vball_tracking
 
-# Train on Titan-RTX Turing
-export CUDA_VISIBLE_DEVICES=0,1
-export WORLD_SIZE=1
-#export CUDA_VISIBLE_DEVICES=0
-#unset WORLD_SIZE
+#export CUDA_VISIBLE_DEVICES=0,1
+#export WORLD_SIZE=1
+export CUDA_VISIBLE_DEVICES=0
+unset WORLD_SIZE
 
 EXP=yolox_x_corners_players_referee
 DSET="v11_player_ref_court"
-TAG=$DSET
+TAG=""
 
 CKPT=/mnt/f/output/ByteTrack/YOLOX_outputs/yolox_x_mot_ablation/latest_ckpt.pth.tar
 CKPT=/mnt/h/checkpoints/bytetrack/bytetrack_x_mot17.pth.tar
-#	-d 1 -b 4 --fp16 \
+#	-d 2 -b 8 -o --fp16 \
+
 python3 tools/train.py \
-	-d 2 -b 8 --fp16 \
+	-d 1 -b 4 -o --fp16 \
 	-f exps/example/mot/${EXP}.py \
 	-c $CKPT \
         -expn ${EXP}_${DSET}${TAG} \
 	train_ann /mnt/g/data/vball/fullcourt/trn_${DSET}.json \
 	val_ann /mnt/g/data/vball/fullcourt/val_${DSET}.json \
-	max_epoch 50 no_aug_epochs 8
-
-#	-o \
+	max_epoch 50 no_aug_epochs 8 \
+	data_num_workers 8
--- yolox/core/trainer.py
+++ yolox/core/trainer.py
@@ -98,7 +98,7 @@ class Trainer:
         targets.requires_grad = False
         data_end_time = time.time()
 
-        with torch.cuda.amp.autocast(enabled=self.amp_training):
+        with torch.amp.autocast(enabled=self.amp_training, device_type='cuda'):
             outputs = self.model(inps, targets)
         loss = outputs["total_loss"]
 
